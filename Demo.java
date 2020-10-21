cb6c44a9873a914f86c4d817839ee7e2c75579ed
Rickey Dong <rickeyd2004@gmail.com>
2020-10-21 17:59:27 -0400

    Added arrToString method to Demo.java

--- Demo.java
+++ Demo.java
@@ -10,6 +10,13 @@ public class Demo
     {
       printLoop(Integer.parseInt(args[0]));
     }
+    //int[] five = new int[5]; //testing arrToString
+    //five[0] = 2; //testing arrToString
+    //five[1] = 4; //testing arrToString
+    //five[2] = 3; //testing arrToString
+    //five[3] = 1; //testing arrToString
+    //five[4] = 7; //testing arrToString
+    //System.out.println(arrToString(five)); //should print {2, 4, 3, 1, 7}
   }
   public static void printLoop(int n)
   {
@@ -21,5 +28,23 @@ public class Demo
       }
       System.out.println();
     }
+  }
+  public static String arrToString(int[] arr)
+  {
+    String stringarr = "{";
+    for (int i = 0; i < arr.length; i++)
+    {
+      if (i == arr.length - 1)
+      {
+        stringarr = stringarr + arr[i];
+      }
+      else
+      {
+        stringarr = stringarr + arr[i];
+        stringarr = stringarr + ", ";
+      }
+    }
+    stringarr = stringarr + "}";
+    return stringarr;
   }
 }
